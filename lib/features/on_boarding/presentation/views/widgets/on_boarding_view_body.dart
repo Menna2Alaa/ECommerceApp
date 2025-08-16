769cf8ad715dc1a440bdd490cf004290577c0598
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-08-16 22:04:38 +0300

    dots indicator done

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,11 +1,24 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:e_commerce_app2/core/utilies/app_colors.dart';
 import 'package:e_commerce_app2/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Expanded(child: OnBoardingPageView())]);
+    return Column(
+      children: [
+        Expanded(child: OnBoardingPageView()),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            color: AppColors.primaryColor.withValues(alpha: 0.5),
+            activeColor: AppColors.primaryColor,
+          ),
+        ),
+      ],
+    );
   }
 }
