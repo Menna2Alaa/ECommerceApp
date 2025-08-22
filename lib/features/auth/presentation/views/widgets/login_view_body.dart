b9766f39623c14e0c194508ddfe579638a8b2023
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-08-22 23:25:00 +0300

    Adds social login options to the login view
    Implements social login buttons (Google, Apple, Facebook) to allow users to sign in with their existing accounts.

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,10 +1,12 @@
 import 'package:e_commerce_app2/constants.dart';
 import 'package:e_commerce_app2/core/utilies/app_colors.dart';
+import 'package:e_commerce_app2/core/utilies/app_images.dart';
 import 'package:e_commerce_app2/core/utilies/app_text_styles.dart';
 import 'package:e_commerce_app2/core/widgets/custome_button.dart';
 import 'package:e_commerce_app2/core/widgets/custome_text_form_field.dart';
 import 'package:e_commerce_app2/features/auth/presentation/views/widgets/donot_have_an_Account_widget.dart';
 import 'package:e_commerce_app2/features/auth/presentation/views/widgets/or_divider.dart';
+import 'package:e_commerce_app2/features/auth/presentation/views/widgets/social_login_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -12,41 +14,64 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 24),
-            const CustomeTextFormField(
-              hintText: 'Email',
-              textInputType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 16),
-            const CustomeTextFormField(
-              hintText: 'PassWord',
-              textInputType: TextInputType.visiblePassword,
-              suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'Forgot Password?',
-                  style: AppTextStyles.semiBold13.copyWith(
-                    color: AppColors.lightPrimaryColor,
-                  ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 24),
+              const CustomeTextFormField(
+                hintText: 'Email',
+                textInputType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 16),
+              const CustomeTextFormField(
+                hintText: 'PassWord',
+                textInputType: TextInputType.visiblePassword,
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffC9CECF),
                 ),
-              ],
-            ),
-            const SizedBox(height: 33),
-            CustomeButton(onPressed: () {}, text: 'Sign In'),
-            const SizedBox(height: 33),
-            const DonotHaveAnAccountWidget(),
-            SizedBox(height: 33),
-            OrDivider(),
-          ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Forgot Password?',
+                    style: AppTextStyles.semiBold13.copyWith(
+                      color: AppColors.lightPrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 33),
+              CustomeButton(onPressed: () {}, text: 'Sign In'),
+              const SizedBox(height: 33),
+              const DonotHaveAnAccountWidget(),
+              const SizedBox(height: 33),
+              const OrDivider(),
+              const SizedBox(height: 16),
+              SocialLoginButton(
+                image: Assets.assetsImagesGoogleIcon,
+                title: 'Sign in with google',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 16),
+              SocialLoginButton(
+                image: Assets.assetsImagesApplIcon,
+                title: 'Sign in with apple',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 16),
+              SocialLoginButton(
+                image: Assets.assetsImagesFacebookIcon,
+                title: 'Sign in with facebook',
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
