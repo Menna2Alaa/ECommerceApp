1d227b09fede03ac85f0a3cc96bd4cdfdd4eb9bd
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-09-28 20:53:48 +0300

    implement fetch user data

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,4 +1,7 @@
 abstract class DataBaseService {
   Future<void> addData(
       {required String path, required Map<String, dynamic> data});
+
+  Future<Map<String, dynamic>> getData(
+      {required String path, required String documentId});
 }
