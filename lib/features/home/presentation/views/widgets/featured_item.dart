2cf6b0a43b300a1f80b3736c636bd4784ec9df9e
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-10-02 23:39:10 +0300

    create featured item part three

--- lib/features/home/presentation/views/widgets/featured_item.dart
+++ lib/features/home/presentation/views/widgets/featured_item.dart
@@ -3,7 +3,8 @@ import 'package:e_commerce_app2/core/utilies/app_images.dart';
 import 'package:e_commerce_app2/core/utilies/app_text_styles.dart';
 import 'package:e_commerce_app2/features/home/presentation/views/widgets/featured_item_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart' as svg;
 
 class FeaturedItem extends StatelessWidget {
   const FeaturedItem({super.key});
@@ -18,7 +19,13 @@ class FeaturedItem extends StatelessWidget {
         aspectRatio: 342 / 158,
         child: Stack(
           children: [
-            Image.asset(Assets.assetsImagesWatermelonTest),
+            Positioned(
+                right: 0,
+                bottom: 0,
+                top: 0,
+                left: itemWidth * 0.4,
+                child: SvgPicture.asset(Assets.assetsImagesPageViewItem2Image,
+                    fit: BoxFit.fill)),
             SizedBox(
               width: itemWidth * 0.5,
               child: Stack(
@@ -28,7 +35,8 @@ class FeaturedItem extends StatelessWidget {
                       alignment: Alignment.center,
                       transform: Matrix4.rotationY(math.pi),
                       child: Image(
-                        image: Svg(Assets.assetsImagesFeaturedItemBackground),
+                        image:
+                            svg.Svg(Assets.assetsImagesFeaturedItemBackground),
                         fit: BoxFit.fill,
                       ),
                     ),
